64def787be87fca7bf0d15e88d9c4f69bbf8578a
Ell-716 <elenabai.2021@gmail.com>
2024-10-06 16:20:56 +0200

    Bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,22 +7,35 @@ def load_data(file_path):
         return json.load(handle)
 
 
+def load_html(html_template):
+    with open(html_template, "r") as file:
+        return file.read()
+
+
 def get_animals(animals_data):
+    output = ""
     for animal in animals_data:
-        print(f"Name: {animal["name"]}")
-        print(f"Diet: {animal["characteristics"]["diet"]}")
-        print(f"Location: {animal["locations"][0]}")
+        output += f"\nName: {animal["name"]}\n"
+        output += f"Diet: {animal["characteristics"]["diet"]}\n"
+        output += f"Location: {animal["locations"][0]}\n"
 
         if "type" in animal["characteristics"]:
-            print(f"Type: {animal["characteristics"]["type"]}")
+            output += f"Type: {animal["characteristics"]["type"]}\n"
+    return output
 
-        print()
+
+def new_animals_file(replaced_data):
+    with open("animals.html", "w") as new_file:
+        return new_file.write(replaced_data)
 
 
 def main():
     animals_data = load_data('animals_data.json')
-    get_animals(animals_data)
+    html_data = load_html("animals_template.html")
+    output = get_animals(animals_data)
+    replaced_data = html_data.replace("__REPLACE_ANIMALS_INFO__", output)
+    animals_html = new_animals_file(replaced_data)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
