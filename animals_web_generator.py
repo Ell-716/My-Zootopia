e312c57aa53907fd9caa266f0de26683184c2391
Ell-716 <elenabai.2021@gmail.com>
2024-10-06 17:32:56 +0200

    Formatting and adding serialize_animal function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,29 +8,37 @@ def load_data(file_path):
 
 
 def load_html(html_template):
+    """ Loads an HTML template """
     with open(html_template, "r") as file:
         return file.read()
 
 
+def serialize_animal(animal_obj):
+    """ Serializes a single animal object into HTML format """
+    output = '<li class="cards__item">\n'
+    output += f'<div class="card__title">{animal_obj["name"]}</div>\n'
+    output += '<p class="card__text">\n'
+    output += f"<strong>Diet:</strong> {animal_obj['characteristics']['diet']}<br/>\n"
+    output += f"<strong>Location:</strong> {animal_obj['locations'][0]}<br/>\n"
+
+    if "type" in animal_obj["characteristics"]:
+        output += f"<strong>Type:</strong> {animal_obj['characteristics']['type']}<br/>\n"
+
+    output += '</p>\n'
+    output += '</li>\n'
+    return output
+
+
 def get_animals(animals_data):
+    """ Serializes all animals into HTML format """
     output = ""
     for animal in animals_data:
-        output += '<li class="cards__item">'
-        output += '<div class ="card__title">'
-        output += f"\n{animal["name"]}<br/>\n"
-        output += '</div>'
-        output += '<p class="card__text">'
-        output += f"<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n"
-        output += f"<strong>Location:</strong> {animal["locations"][0]}<br/>\n"
-
-        if "type" in animal["characteristics"]:
-            output += f"<strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n"
-        output += '</p>\n'
-        output += '</li>\n'
+        output += serialize_animal(animal)
     return output
 
 
 def new_animals_file(replaced_data):
+    """ Writes the final HTML data to a file """
     with open("animals.html", "w") as new_file:
         return new_file.write(replaced_data)
 
